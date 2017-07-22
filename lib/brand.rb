7fe768f1ade2bf420e914e190075f220babac646
kitet <kitetnet@gmail.com>
2017-07-23 00:55:26 +0300

    added callback method to capitalize all inputs before submission

--- lib/brand.rb
+++ lib/brand.rb
@@ -5,4 +5,14 @@ class Brand < ActiveRecord::Base
     validates(:name, {:presence => true, :length => { :minimum => 0 }})
     validates(:category, {:presence => true, :length => { :minimum => 0 }})
     validates(:gender, {:presence => true, :length => { :minimum => 0 }})
+
+    before_save(:capitalize_properties)
+
+    private
+
+    def capitalize_properties
+        self.name=(name().capitalize())
+        self.category=(category().capitalize())
+        self.gender=(gender().capitalize())
+    end
 end
