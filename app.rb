4f4365b78a1bcf4d5825845f9b92b9eb8bf47eae
kitet <kitetnet@gmail.com>
2017-07-22 10:23:11 +0300

    added two pages using sinatra get (brands and stores)

--- app.rb
+++ app.rb
@@ -7,3 +7,12 @@ Dir[File.dirname(__FILE__)+"/../lib/*.rb"].each {|file| require file}
 get '/' do
     erb :index
 end
+
+get '/brands' do
+    erb :brands
+end
+
+get '/stores' do
+    @stores=Store.all
+    erb :stores
+end
