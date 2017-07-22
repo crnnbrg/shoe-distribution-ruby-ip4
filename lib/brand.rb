a5310db6c1f157c92c9531b5a9d1c2505ce2a9d0
kitet <kitetnet@gmail.com>
2017-07-23 00:33:40 +0300

    added validation for entries

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,4 +1,8 @@
 class Brand < ActiveRecord::Base
     has_many :stocks
     has_many :stores, through: :stocks
+
+    validates(:name, {:presence => true, :length => { :minimum => 0 }})
+    validates(:category, {:presence => true, :length => { :minimum => 0 }})
+    validates(:gender, {:presence => true, :length => { :minimum => 0 }})
 end
