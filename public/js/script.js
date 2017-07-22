409e3ec5a1224919698b9500f3c6349469e6ef65
kitet <kitetnet@gmail.com>
2017-07-22 16:39:04 +0300

    added code to show the new_brand_form on click

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
-    $("#addnewstore").click(function(){
-        $("#new_store_form").fadeIn(2000);
+    $(".newstore").click(function(){
+        $(".new_store_form").fadeIn(1500);
+    });
+
+    $(".submit_store").click(function(){
+        $(".new_store_form").fadeOut(1500);
+    });
+
+    $("#add_brand_btn").click(function(){
+        $(".new_brand_form").fadeIn(1500);
+    });
+
+    $(".submit_brand").click(function(){
+        $(".new_brand_form").fadeOut(1500);
     });
 });
