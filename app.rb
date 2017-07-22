241cb56d166184e7551918ec504ff5aa081aabc6
kitet <kitetnet@gmail.com>
2017-07-22 16:37:50 +0300

    added method to save brands to database

--- app.rb
+++ app.rb
@@ -9,10 +9,28 @@ get '/' do
 end
 
 get '/brands' do
+    @brands=Brand.all
     erb :brands
 end
 
 get '/stores' do
     @stores=Store.all
     erb :stores
+end
+
+post '/stores/new' do
+    name=params.fetch('name')
+    telephone=params.fetch('telephone')
+    address=params.fetch('address')
+    country=params.fetch('country')
+    Store.create(name: name,telephone:telephone,address:address,country:country)
+    redirect '/stores'
+end
+
+post '/brands/new' do
+    name=params.fetch('name')
+    category=params.fetch('category')
+    gender=params.fetch('gender')
+    Brand.create(name: name,category:category,gender:gender)
+    redirect '/brands'
 end
