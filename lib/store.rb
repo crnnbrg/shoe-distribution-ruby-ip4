d8585038cbc903c6b0ef60ccc6fd71ee71a767ec
kitet <kitetnet@gmail.com>
2017-07-23 00:34:03 +0300

    added validation of inputs

--- lib/store.rb
+++ lib/store.rb
@@ -1,4 +1,10 @@
 class Store < ActiveRecord::Base
     has_many :stocks
     has_many :brands, through: :stocks
+
+
+    validates(:name, {:presence => true, :length => { :minimum => 0 }})
+    validates(:address, {:presence => true, :length => { :minimum => 0 }})
+    validates(:telephone, {:presence => true, :length => { :minimum => 0 }})
+    validates(:country, {:presence => true, :length => { :minimum => 0 }})
 end
