593f59dec3f323bacdc6d0a7326dc030f13fb077
EdwardMudaida <eddieigo8@gmail.com>
2017-07-25 18:17:49 +0300

    Adds custom styling for the homepage

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,18 +1,18 @@
 class Brand < ActiveRecord::Base
-    has_many :stocks
-    has_many :stores, through: :stocks
+  has_many :stocks
+  has_many :stores, through: :stocks
 
-    validates(:name, {:presence => true, :length => { :minimum => 0 }})
-    validates(:category, {:presence => true, :length => { :minimum => 0 }})
-    validates(:gender, {:presence => true, :length => { :minimum => 0 }})
+  validates(:name, {:presence => true, :length => { :minimum => 0 }})
+  validates(:category, {:presence => true, :length => { :minimum => 0 }})
+  validates(:gender, {:presence => true, :length => { :minimum => 0 }})
 
-    before_save(:capitalize_properties)
+  before_save(:capitalize_properties)
 
-    private
+  private
 
-    def capitalize_properties
-        self.name=(name().capitalize())
-        self.category=(category().capitalize())
-        self.gender=(gender().capitalize())
-    end
+  def capitalize_properties
+    self.name=(name().capitalize())
+    self.category=(category().capitalize())
+    self.gender=(gender().capitalize())
+  end
 end
